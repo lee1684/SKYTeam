50e465e6051661047d694b5e6f909d986242f732
leejeongjun <backbone94@ajou.ac.kr>
2024-04-11 20:17:06 +0900

    feat: 스프링부트 초기 설정

---
+++ back-end/src/main/java/kr/co/ssalon/SsalonApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ssalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsalonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsalonApplication.class, args);
+    }
+
+}
