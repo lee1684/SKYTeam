2f5404195c5fea0b294263d97f0cb25de8626462
sys7498 <sys7498@ajou.ac.kr>
2024-04-12 02:27:16 +0900

    type: feat setting angular, working mobile ticket viewer

--- front-end/mobile-ticket/src/app/app.component.ts
+++ front-end/mobile-ticket/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SceneSettingComponent } from './scene-setting/scene-setting.component';
+import { Scene } from 'three';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SceneSettingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mobile-ticket';
+
 }
