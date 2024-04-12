244f1a327d30a379f46caafe79f07a18d548a3e5
sys7498 <sys7498@ajou.ac.kr>
2024-04-12 14:18:46 +0900

    fix: code fix

--- front-end/mobile-ticket/src/app/app.component.ts
+++ front-end/mobile-ticket/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SceneSettingComponent } from './scene-setting/scene-setting.component';
 import { Scene } from 'three';
+import { MobileTicketEditorComponent } from './mobile-ticket-editor/mobile-ticket-editor.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SceneSettingComponent],
+  imports: [RouterOutlet, SceneSettingComponent, MobileTicketEditorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
