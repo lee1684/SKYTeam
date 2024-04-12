7d28968bddebc6895abdfe05c7e796e6bc6231bc
sys7498 <sys7498@ajou.ac.kr>
2024-04-12 14:18:46 +0900

    fix: code fix

---
+++ front-end/mobile-ticket/src/app/mobile-ticket-editor/mobile-ticket-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-ticket-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-ticket-editor.component.html',
+  styleUrl: './mobile-ticket-editor.component.scss'
+})
+export class MobileTicketEditorComponent {
+
+}
