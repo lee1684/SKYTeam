c4a3c44014b9b819169c4bf926b8528c427b594a
sys7498 <sys7498@ajou.ac.kr>
2024-04-14 03:38:33 +0900

    fix: renew ticketViewer

--- front-end/mobile-ticket/src/app/ssalon-component/simple-button/simple-button.component.ts
+++ front-end/mobile-ticket/src/app/ssalon-component/simple-button/simple-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-simple-button',
@@ -9,4 +9,5 @@ import { Component, Input } from '@angular/core';
 })
 export class SimpleButtonComponent {
   @Input() label: string = '';
+  @Output() public readonly onClickEvent = new EventEmitter();
 }
---
+++ front-end/mobile-ticket/src/app/ticket/mobile-ticket-editor/mobile-ticket-editor.component.ts
@@ -0,0 +1,42 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { SimpleButtonComponent } from '../../ssalon-component/simple-button/simple-button.component';
+import { NgFor } from '@angular/common';
+import { MobileTicket } from '../../service/mobile-ticket';
+import { ScenegraphService } from '../../service/scenegraph.service';
+
+@Component({
+  selector: 'app-mobile-ticket-editor',
+  standalone: true,
+  imports: [SimpleButtonComponent, NgFor],
+  templateUrl: './mobile-ticket-editor.component.html',
+  styleUrl: './mobile-ticket-editor.component.scss',
+})
+export class MobileTicketEditorComponent {
+  @ViewChild('temp', { static: true }) temp: ElementRef | null = null;
+  public editFeatureLabels: string[] = ['Text', 'Picture', 'Draw'];
+  constructor(private _sceneGraphService: ScenegraphService) {}
+
+  ngAfterViewInit(): void {}
+  public onClickFocusFrontButton(): void {
+    this._sceneGraphService.focusFront();
+  }
+
+  public onClickEditFeatureButton(label: string): void {
+    switch (label) {
+      case 'Text':
+        this._sceneGraphService.mobileTicket!.frontSide!.addText();
+        break;
+      case 'Picture':
+        //this._sceneGraphService.mobileTicket!.frontSide!.addPicture();
+        break;
+      case 'Draw':
+        //this._sceneGraphService.mobileTicket!.frontSide!.addDraw();
+        break;
+    }
+  }
+  public onClickButton() {
+    this.temp!.nativeElement.appendChild(
+      this._sceneGraphService.mobileTicket!.frontSide!.fabricCanvas!.getElement()
+    );
+  }
+}
