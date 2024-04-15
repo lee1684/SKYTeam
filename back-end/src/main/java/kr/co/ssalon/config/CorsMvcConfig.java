4fd02ea1ae3391ca33526b91ff02db1d5f186a26
leejeongjun <backbone94+laptop@ajou.ac.kr>
2024-04-15 23:54:02 +0900

    feat: config 추가
    - CorsMvcConfig: CORS 설정
    - SecurityConfig: 스프링 시큐리티 설정 (사용자 인증/인가 및 보안 설정)

---
+++ back-end/src/main/java/kr/co/ssalon/config/CorsMvcConfig.java
@@ -0,0 +1,14 @@
+package kr.co.ssalon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173");
+    }
+}
