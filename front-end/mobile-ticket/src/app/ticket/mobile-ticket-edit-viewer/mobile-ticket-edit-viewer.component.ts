2536cd94a9c38b88ae11e233b181332731ab379f
sys7498 <sys7498@ajou.ac.kr>
2024-04-18 09:48:54 +0900

    fabric js working

--- front-end/mobile-ticket/src/app/ticket/mobile-ticket-edit-viewer/mobile-ticket-edit-viewer.component.ts
+++ front-end/mobile-ticket/src/app/ticket/mobile-ticket-edit-viewer/mobile-ticket-edit-viewer.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { fabric } from 'fabric';
 @Component({
   selector: 'app-mobile-ticket-edit-viewer',
   standalone: true,
   imports: [],
   templateUrl: './mobile-ticket-edit-viewer.component.html',
-  styleUrl: './mobile-ticket-edit-viewer.component.scss'
+  styleUrl: './mobile-ticket-edit-viewer.component.scss',
 })
 export class MobileTicketEditViewerComponent {
-
+  public canvas: fabric.Canvas | null = null;
+  constructor() {}
+  public ngAfterViewInit(): void {
+    this.initFabric();
+  }
+  initFabric(): void {
+    this.canvas = new fabric.Canvas('edit-canvas');
+    this.canvas.on({
+      'touch:gesture': function () {},
+      'touch:drag': function () {},
+      'touch:orientation': function () {},
+      'touch:shake': function () {},
+      'touch:longpress': function () {},
+    });
+    console.log(this.canvas);
+    let a = new fabric.Text('Hello', {
+      left: 100,
+      top: 100,
+    });
+    a.perPixelTargetFind = true;
+    this.canvas.add(a);
+  }
 }
