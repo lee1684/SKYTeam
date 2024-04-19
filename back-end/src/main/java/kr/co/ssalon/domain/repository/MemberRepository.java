f600dcbe1c81332a6394a0dd9727ade6181f176c
leejeongjun <backbone94+laptop@ajou.ac.kr>
2024-04-20 00:02:39 +0900

    refactor: 회원레포지토리와 회원서비스 수정

--- back-end/src/main/java/kr/co/ssalon/domain/repository/MemberRepository.java
+++ back-end/src/main/java/kr/co/ssalon/domain/repository/MemberRepository.java
@@ -4,9 +4,11 @@ import kr.co.ssalon.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
-    Member findByUsername(String username);
+    Optional<Member> findByUsername(String username);
 
 }
--- back-end/src/main/java/kr/co/ssalon/domain/service/MemberService.java
+++ back-end/src/main/java/kr/co/ssalon/domain/service/MemberService.java
@@ -3,8 +3,11 @@ package kr.co.ssalon.domain.service;
 import kr.co.ssalon.domain.entity.Member;
 import kr.co.ssalon.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,25 +17,36 @@ public class MemberService {
 
     @Transactional
     public Long register(String username, String email, String role) throws Exception {
-        validationMember(username);
-        Member member = Member.builder()
-                .username(username)
-                .email(email)
-                .role(role)
-                .build();
+        validationUsername(username);
+        Member member = Member.createMember(username, email, role);
         memberRepository.save(member);
         return member.getId();
     }
-    private void validationMember(String name) throws Exception {
-        Member findMember = memberRepository.findByUsername(name);
-        if (findMember != null) {
+    @Transactional
+    public void oauthUpdate(String username, String email, String role) throws BadRequestException {
+        Optional<Member> findMember = memberRepository.findByUsername(username);
+        Member member = validaitonMember(findMember);
+        member.changeEmail(email);
+        member.changeRole(role);
+    }
+
+
+    public Member findMember(String username) throws BadRequestException {
+        Optional<Member> findMember = memberRepository.findByUsername(username);
+        Member member = validaitonMember(findMember);
+        return member;
+    }
+
+    private Member validaitonMember(Optional<Member> member) throws BadRequestException {
+        if (member.isPresent()) {
+            return member.get();
+        }else
+            throw new BadRequestException("해당 회원을 찾을 수 없습니다");
+    }
+    private void validationUsername(String username) throws Exception {
+        Optional<Member> findMember = memberRepository.findByUsername(username);
+        if (findMember.isPresent()) {
             throw new Exception("이미 해당 소셜 로그인을 한 회원이 DB에 존재합니다. 업데이트를 진행합니다.");
         }
-    }
-    @Transactional
-    public void oauthUpdate(String name, String email, String role) {
-        Member findMember = memberRepository.findByUsername(name);
-        findMember.changeEmail(email);
-        findMember.changeRole(role);
     }
 }
