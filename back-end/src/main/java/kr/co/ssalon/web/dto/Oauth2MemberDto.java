15d530b09da16eb0b207dff32bf61314d8a91694
YOOSUSANG <dbtntkd456@naver.com>
2024-04-20 00:01:41 +0900

    refactor: Oauth2MemberDto 수정

--- back-end/src/main/java/kr/co/ssalon/web/dto/Oauth2MemberDto.java
+++ back-end/src/main/java/kr/co/ssalon/web/dto/Oauth2MemberDto.java
@@ -1,19 +1,28 @@
 package kr.co.ssalon.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import kr.co.ssalon.oauth2.OAuth2Response;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Oauth2MemberDto {
 
+    @NotBlank
     private String username;
+    @NotBlank
     private String email;
+    @NotBlank
     private String role;
 
-    public Oauth2MemberDto(String username, String role) {
-        this.username = username;
-        this.role = role;
+    public Oauth2MemberDto(OAuth2Response oAuth2Response) {
+        this.username =  oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        this.email = oAuth2Response.getEmail();
+        this.role = "ROLE_USER";
     }
-
 }
