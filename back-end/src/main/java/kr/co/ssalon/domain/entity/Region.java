2698ed683fdd44de5f0fb55c31aa4dc443119fe0
leejeongjun <backbone94@ajou.ac.kr>
2024-04-26 05:25:50 +0900

    fix: 누락된 지역 추가

--- back-end/src/main/java/kr/co/ssalon/domain/entity/Region.java
+++ back-end/src/main/java/kr/co/ssalon/domain/entity/Region.java
@@ -9,7 +9,11 @@ public enum Region {
     GYEONGGIDO("경기도"),
     GANGWONDO("강원도"),
     GYEONGSANGNAMDO("경상남도"),
-    GYEONGSANGBUKDO("경상북도");
+    GYEONGSANGBUKDO("경상북도"),
+    JEOLLABUKDO("전라북도"),
+    JEOLLANAMDO("전라남도"),
+    CHUNGCHEONGNAMDO("충청남도"),
+    CHUNGCHEONGBUKDO("충청북도");
 
     private final String localName;
 
