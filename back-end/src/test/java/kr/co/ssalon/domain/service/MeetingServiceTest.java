43f6514b4b801e8b9148089b23d8111add6d4d7b
leejeongjun <backbone94@ajou.ac.kr>
2024-04-27 13:59:22 +0900

    test: MeetingServiceTest 테스트 코드 추가
    - @ExtendWith(MockitoExtension.class) vs. @ExtendWith(SpringExtension.class)
    - 좀더 가벼운 컨텍스트를 로드하는 MockitoExtension.class 사용

---
+++ back-end/src/test/java/kr/co/ssalon/domain/service/MeetingServiceTest.java
@@ -0,0 +1,68 @@
+package kr.co.ssalon.domain.service;
+
+import kr.co.ssalon.domain.entity.Category;
+import kr.co.ssalon.domain.entity.Meeting;
+import kr.co.ssalon.domain.entity.Region;
+import kr.co.ssalon.domain.repository.MeetingRepository;
+import kr.co.ssalon.web.dto.MeetingSearchCondition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MeetingServiceTest {
+
+    @Mock
+    private MeetingRepository meetingRepository;
+
+    @InjectMocks
+    private MeetingService meetingService;
+
+    @Test
+    @DisplayName("meetingService.getMoims 메소드 테스트")
+    public void 모임목록조회() {
+        // given
+
+        // "운동" 카테고리 Mock 객체 생성
+        Category category = mock(Category.class);
+        when(category.getName()).thenReturn("운동");
+
+        // "운동", "서울특별시" 모임 Mock 객체 생성
+        Meeting meeting = mock(Meeting.class);
+        when(meeting.getCategory()).thenReturn(category);
+        when(meeting.getLocation()).thenReturn(Region.SEOUL.getLocalName());
+
+        // 모임 목록 필터("운동", "서울특별시") 객체 생성
+        MeetingSearchCondition meetingSearchCondition = MeetingSearchCondition.builder()
+                .categoryName("운동")
+                .region(Region.SEOUL)
+                .build();
+
+        // Pageable 객체 생성
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        // "운동", "서울특별시" 모임 Mock 객체에 대한 Page 객체 생성
+        Page<Meeting> meetingsPage = new PageImpl<>(Collections.singletonList(meeting));
+        when(meetingRepository.searchMoims(meetingSearchCondition, pageable)).thenReturn(meetingsPage);
+
+        // when (모임 목록 조회)
+        Page<Meeting> result = meetingService.getMoims(meetingSearchCondition, pageable);
+
+        // then (조회한 모임에 대한 검증)
+        assertThat(result).isNotNull();
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent().get(0).getLocation()).isEqualTo("서울특별시");
+        assertThat(result.getContent().get(0).getCategory().getName()).isEqualTo("운동");
+    }
+}
