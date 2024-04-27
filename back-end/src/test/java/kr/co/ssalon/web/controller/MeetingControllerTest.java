acb028b109cdaa55f572fde16e425ad1b16bb589
leejeongjun <backbone94@ajou.ac.kr>
2024-04-27 19:51:44 +0900

    test: MeetingControllerTest 테스트 코드 추가
    - @SpringBootTest vs. @WebMvcTest(MeetingController.class)
    - @SpringBootTest: 스프링의 모든 빈을 로드. 통합 테스트에 주로 사용
    - @WebMvcTest: 웹 계층 빈들만 로드. 단위 테스트에 주로 사용
    - @WebMvcTest 선택 -> Service는 @MockBean을 통해 주입 받아야 함
    - @WithMockUser(username = "test"): OAuth2 테스트용 인증 정보 사용

---
+++ back-end/src/test/java/kr/co/ssalon/web/controller/MeetingControllerTest.java
@@ -0,0 +1,78 @@
+package kr.co.ssalon.web.controller;
+
+import kr.co.ssalon.domain.entity.Category;
+import kr.co.ssalon.domain.entity.Meeting;
+import kr.co.ssalon.domain.entity.Region;
+import kr.co.ssalon.domain.service.MeetingService;
+import kr.co.ssalon.web.dto.MeetingSearchCondition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MeetingController.class)
+public class MeetingControllerTest {
+
+    @MockBean
+    private MeetingService meetingService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("모임 목록 조회 API(GET /moims) 테스트")
+    @WithMockUser(username = "test")
+    public void 모임목록조회API() throws Exception {
+        // given
+
+        // "운동" 카테고리 Mock 객체 생성
+        Category category = mock(Category.class);
+        when(category.getName()).thenReturn("운동");
+
+        // "운동", "서울특별시" 모임 Mock 객체 생성
+        Meeting meeting = mock(Meeting.class);
+        when(meeting.getCategory()).thenReturn(category);
+        when(meeting.getLocation()).thenReturn(Region.SEOUL.getLocalName());
+
+        // 모임 목록 필터("운동", "서울특별시") 객체 생성
+        MeetingSearchCondition meetingSearchCondition = MeetingSearchCondition.builder()
+                .categoryName("운동")
+                .region(Region.SEOUL)
+                .build();
+
+        // Pageable 객체 생성
+        Pageable pageable = PageRequest.of(0, 10);
+
+        // "운동", "서울특별시" 모임 Mock 객체에 대한 Page 객체 생성
+        Page<Meeting> meetingsPage = new PageImpl<>(Collections.singletonList(meeting));
+        when(meetingService.getMoims(meetingSearchCondition, pageable)).thenReturn(meetingsPage);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/moims")
+                .param("categoryName", "운동")
+                .param("region", "SEOUL")
+                .param("page", "0")
+                .param("size", "10"));
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].location", is("서울특별시")));
+
+    }
+}
