83838785c7474ccc09509586dfcd328e7a65ab89
pileuszu_pc <pileuszu@gmail.com>
2024-04-29 03:00:06 +0900

    feat: QrLink 생성, 조회, 검사 생성
    - MemberMeeting.java: 필드값에 String QrLink 생성
    - MemberMeetingRepository.java: findByMemberAndMeeting 쿼리문 생성
    - QrController.java: QR, 생성, 조회, 검증 추가
    - QrService.java: QrController.java에 필요한 함수 추가

---
+++ back-end/src/main/java/kr/co/ssalon/web/controller/QrController.java
@@ -0,0 +1,52 @@
+package kr.co.ssalon.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import kr.co.ssalon.domain.service.QrService;
+import kr.co.ssalon.oauth2.CustomOAuth2Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag(name = "QR")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class QrController {
+
+    private final QrService qrService;
+
+    @Operation(summary = "QR 생성/조회")
+    @ApiResponse(value = {
+            @ApiResponse(responseCode =  "200", description = "QR 생성/조회 성공"),
+    })
+    @GetMapping("/tickets/{moimId}/{userId}/link")
+    public ResponseEntity<?> getQrLink(@AuthenticationPrincipal CustomOAuth2Member customOAuth2Member, @PathVariable Long moimId, @PathVariable Long userId) {
+        try {
+            return ResponseEntity.ok().body(qrService.getQrLink(customOAuth2Member, moimId, userId));
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Operation(summary = "QR 검증")
+    @ApiResponse(value = {
+            @ApiResponse(responseCode =  "200", description = "QR 검증 성공"),
+    })
+    @PostMapping("/tickets/{moimId}/{userId}/link")
+    public ResponseEntity<?> checkQrLink(@AuthenticationPrincipal CustomOAuth2Member customOAuth2Member, @PathVariable Long moimId, @PathVariable Long userId, @RequestParam("file") MultipartFile file) {
+        try {
+            return ResponseEntity.ok().body(qrService.checkQrLink(customOAuth2Member, moimId, userId, file));
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+}
