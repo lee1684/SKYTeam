619836520d0b0bc548ac3b6be2d81e7657d10f52
jihwankim129 <pillaret20@ajou.ac.kr>
2024-05-02 15:41:31 +0900

    feat: 미팅 컨트롤러 생성
    MeetingController.java: 미팅 컨트롤러 생성

--- back-end/src/main/java/kr/co/ssalon/web/controller/MeetingController.java
+++ back-end/src/main/java/kr/co/ssalon/web/controller/MeetingController.java
@@ -59,4 +59,95 @@ public class MeetingController {
         Page<MeetingDTO> moimsDto = moims.map(meeting -> new MeetingDTO(meeting));
         return ResponseEntity.ok().body(moimsDto.getContent());
     }
+
+
+    @Operation(summary = "테스트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "테스트 성공"),
+    })
+    @GetMapping("/api/test")
+    public ResponseEntity<?> getTest(@AuthenticationPrincipal CustomOAuth2Member customOAuth2Member) {
+        return ResponseEntity.ok().body("123");
+    }
+
+    // 모임 개설
+    // 사용자 JWT, 개설할 모임의 정보
+    // 성공/실패 여부, 개설된 모임의 ID
+    @Operation(summary = "모임 개설")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "모임 개설 성공"),
+    })
+    @PostMapping("/moims")
+    public ResponseEntity<?> createMoim(@AuthenticationPrincipal CustomOAuth2Member customOAuth2Member, @RequestBody MeetingDTO meetingDTO) {
+        try {
+            return ResponseEntity.ok().body(meetingService.createMoim(customOAuth2Member, meetingDTO));
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 모임 정보 조회
+    // 사용자 JWT
+    // 성공/실패 여부, 모임 관련 정보 데이터
+    // (소속 여부에 따라 변동) -> ?
+    @Operation(summary = "모임 정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "모임 정보 조회 성공"),
+    })
+    @GetMapping("/moims/{moimId}")
+    public ResponseEntity<?> getMoim(@PathVariable Long moimId, @AuthenticationPrincipal CustomOAuth2Member customOAuth2Member) {
+        try {
+            return ResponseEntity.ok().body(meetingService.getMoim(customOAuth2Member, moimId));
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 모임 정보 수정
+    // 사용자 JWT, 수정할 모임의 정보
+    // 성공/실패 여부
+    @Operation(summary = "모임 정보 수정")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "모임 정보 수정 성공"),
+    })
+    @PatchMapping("/moims/{moimId}")
+    public ResponseEntity<?> updateMoim(@PathVariable Long moimId, @AuthenticationPrincipal CustomOAuth2Member customOAuth2Member, MeetingDTO meetingDTO) {
+        try {
+            return ResponseEntity.ok().body(meetingService.updateMoim(customOAuth2Member, moimId, meetingDTO));
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 모임 해산
+    // 사용자 JWT
+    // 성공/실패 여부
+    @Operation(summary = "모임 해산")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "모임 해산 성공"),
+    })
+    @DeleteMapping("/moims/{moimId}")
+    public ResponseEntity<?> deleteMoim(@PathVariable Long moimId, @AuthenticationPrincipal CustomOAuth2Member customOAuth2Member) {
+        try {
+            return ResponseEntity.ok().body(meetingService.deleteMoim(customOAuth2Member, moimId));
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 모임 참가자 목록 조회
+    // 사용자 JWT
+    // 성공/실패 여부, 모임 참가자 목록
+    @Operation(summary = "모임 참가자 목록 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "모임 참가자 목록 조회"),
+    })
+    @GetMapping("/moims/{moimId}/users")
+    public ResponseEntity<?> getUsers(@PathVariable Long moimId, @AuthenticationPrincipal CustomOAuth2Member customOAuth2Member) {
+        try {
+            return ResponseEntity.ok().body(meetingService.getUsers(customOAuth2Member, moimId));
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
