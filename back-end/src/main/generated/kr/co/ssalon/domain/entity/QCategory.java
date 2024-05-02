9f5bf31e669601cbb1e9e970ee0b5449a461d027
YOOSUSANG <dbtntkd456@naver.com>
2024-05-03 05:01:25 +0900

    refactor: queryDsl

---
+++ back-end/src/main/generated/kr/co/ssalon/domain/entity/QCategory.java
@@ -0,0 +1,43 @@
+package kr.co.ssalon.domain.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QCategory is a Querydsl query type for Category
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCategory extends EntityPathBase<Category> {
+
+    private static final long serialVersionUID = -1921000440L;
+
+    public static final QCategory category = new QCategory("category");
+
+    public final StringPath description = createString("description");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath imageUrl = createString("imageUrl");
+
+    public final StringPath name = createString("name");
+
+    public QCategory(String variable) {
+        super(Category.class, forVariable(variable));
+    }
+
+    public QCategory(Path<? extends Category> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QCategory(PathMetadata metadata) {
+        super(Category.class, metadata);
+    }
+
+}
+
