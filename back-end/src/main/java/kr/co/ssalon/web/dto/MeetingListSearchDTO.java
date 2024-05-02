ba2d6fa18f333638440dcd4766e677d35d9639e4
leejeongjun <backbone94+laptop@ajou.ac.kr>
2024-05-03 04:51:24 +0900

    feat: MeetingListSearchDTO
    - 모임 개설 시 프론트 측에게 보내주는 MeetingListSearchDTO 클래스 생성
    - 카테고리, 썸네일이 수정 필요

---
+++ back-end/src/main/java/kr/co/ssalon/web/dto/MeetingListSearchDTO.java
@@ -0,0 +1,29 @@
+package kr.co.ssalon.web.dto;
+
+import kr.co.ssalon.domain.entity.Meeting;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MeetingListSearchDTO {
+
+    // 모임Id
+    private Long moimId;
+    // 필터를 위한 타입 카테고리 이름?
+    private String categoryName;
+
+    // 썸네일(증표)?
+    private TicketDTO ticketDTO;
+
+    public MeetingListSearchDTO(Meeting meeting) {
+        this.moimId = meeting.getId();
+        this.categoryName = meeting.getCategory().getName();
+        this.ticketDTO = new TicketDTO(meeting.getTicket());
+    }
+
+}
