26135432ff3becdd0381cd3b0fab3fcc1046f11b
leejeongjun <backbone94@ajou.ac.kr>
2024-05-03 15:28:18 +0900

    index.html 접근 시 인가 과정 임시 생략

--- back-end/src/main/java/kr/co/ssalon/config/SecurityConfig.java
+++ back-end/src/main/java/kr/co/ssalon/config/SecurityConfig.java
@@ -101,6 +101,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html/**").permitAll()
                         .requestMatchers("/api/reissue").permitAll()
                         .requestMatchers("/api/tickets/**").permitAll()
+                        .requestMatchers("/index.html/**").permitAll()
                         .anyRequest().authenticated());
 
         // 세션 설정 : STATELESS
