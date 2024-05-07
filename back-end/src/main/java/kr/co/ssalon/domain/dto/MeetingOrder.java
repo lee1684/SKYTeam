ed8b68a08c9ddf76b14ba424fcc1b18b0baac5a4
leejeongjun <backbone94@ajou.ac.kr>
2024-05-07 18:35:01 +0900

    fix: MeetingFilter -> MeetingOrder로 수정
    - 추가로, swagger 설명 덧붙임

--- back-end/src/main/java/kr/co/ssalon/domain/dto/MeetingOrder.java
+++ back-end/src/main/java/kr/co/ssalon/domain/dto/MeetingOrder.java
@@ -3,14 +3,14 @@ package kr.co.ssalon.domain.dto;
 import lombok.Getter;
 
 @Getter
-public enum MeetingFilter {
+public enum MeetingOrder {
     // 추가 필요
     CAPACITY("인원 수"),
     NUMBER("모임 번호"),
     RECENT("최신순");
     private final String description;
 
-    MeetingFilter(String description) {
+    MeetingOrder(String description) {
         this.description = description;
     }
 }
