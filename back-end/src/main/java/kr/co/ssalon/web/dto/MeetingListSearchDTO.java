1c15168024ff98761c91cb832cfc95a50159ef13
SyingSHY <posti1x38@gmail.com>
2024-05-09 13:45:52 +0900

    fix: TicketDTO NULL 문제 긴급 대응
    + MeetingListSearchDTO: Thumbnail 경로 반환
    + TicketService: Thumbnail 저장명칭 수정

--- back-end/src/main/java/kr/co/ssalon/web/dto/MeetingListSearchDTO.java
+++ back-end/src/main/java/kr/co/ssalon/web/dto/MeetingListSearchDTO.java
@@ -18,12 +18,14 @@ public class MeetingListSearchDTO {
     private String categoryName;
 
     // 썸네일(증표)?
-    private TicketDTO ticketDTO;
+    // private TicketDTO ticketDTO;
+    private String ticketThumb;
 
     public MeetingListSearchDTO(Meeting meeting) {
         this.moimId = meeting.getId();
         this.categoryName = meeting.getCategory().getName();
-        this.ticketDTO = meeting.getTicket() == null ? null : new TicketDTO(meeting.getTicket());
+        // this.ticketDTO = meeting.getTicket() == null ? null : new TicketDTO(meeting.getTicket());
+        this.ticketThumb = "https://test-bukkit-240415.s3.ap-northeast-2.amazonaws.com/Thumbnails/" + this.moimId + "/Thumb-" + this.moimId + ".png";
     }
 
 }
