0812596506ad88e7dac050a137da93b21a37dc8b
pileuszu <pileuszu@gmail.com>
2024-05-16 19:04:28 +0900

    feat: 모임 리스트 API 호출시 모임 title 주기
    - MeetingHomeSearchDTO.java: 모임 리스트 API 호출시 모임 title 주기
    - MeetingListSearchDTO.java: 모임 리스트 API 호출시 모임 title 주기

--- back-end/src/main/java/kr/co/ssalon/web/dto/MeetingHomeSearchDTO.java
+++ back-end/src/main/java/kr/co/ssalon/web/dto/MeetingHomeSearchDTO.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class MeetingHomeSearchDTO {
     // 모임Id
     private Long moimId;
+    private String meetingTitle;
     private String backgroundColor;
     // 썸네일(증표)?
     // private TicketDTO ticketDTO;
@@ -20,6 +21,7 @@ public class MeetingHomeSearchDTO {
 
     public MeetingHomeSearchDTO(Meeting meeting) {
         this.moimId = meeting.getId();
+        this.meetingTitle = meeting.getTitle();
         this.backgroundColor = meeting.getBackgroundColor();
         // this.ticketDTO = meeting.getTicket() == null ? null : new TicketDTO(meeting.getTicket());
         this.ticketThumb = "https://test-bukkit-240415.s3.ap-northeast-2.amazonaws.com/Thumbnails/" + this.moimId + "/Thumb-" + this.moimId + ".png";
