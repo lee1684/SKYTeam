bc74187630362e96c934179f3d4cabc53089d7a7
leejeongjun <backbone94@ajou.ac.kr>
2024-05-20 21:50:40 +0900

    fix: stop.sh 수정

--- back-end/scripts/stop.sh
+++ back-end/scripts/stop.sh
@@ -9,6 +9,7 @@ TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "jar 파일의 현재 프로세스 ID: $CURRENT_PID" >> $DEPLOY_LOG
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
