0ea9bfdbd8df9b7f6ca8371e8749e274bff01287
jihwankim129 <pillaret20@ajou.ac.kr>
2024-05-27 17:04:00 +0900

    feat: 이메일로 멤버 정보 조회 메소드 추가
    - MemberEmailDTO.java: email 을 받기 위한 DTO 추가
    - MemberRepository.java: findByEmail로 DB 조회하는 코드 추가
    - MemberService.java: 이메일로 멤버를 조회하는 서비스단 코드 추가
    - UserController.java: 이메일로 멤버를 조회하는 컨트롤러단 코드 추가

---
+++ back-end/src/main/java/kr/co/ssalon/web/dto/MemberEmailDTO.java
@@ -0,0 +1,17 @@
+package kr.co.ssalon.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberEmailDTO {
+
+    private String email;
+
+}
+
