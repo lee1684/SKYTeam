f022e7385e20012060618888069548f95a372dca
SyingSHY <posti1x38@gmail.com>
2024-05-28 22:17:21 +0900

    feat: Recommender 구현

---
+++ back-end/src/main/java/kr/co/ssalon/domain/service/AwsLambdaService.java
@@ -0,0 +1,83 @@
+package kr.co.ssalon.domain.service;
+
+import com.google.gson.JsonObject;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AwsLambdaService {
+
+    private final WebClient webClient;
+
+    public AwsLambdaService() {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://ssalon.co.kr") // 기본 URL 설정
+                .build();
+    }
+
+    public Mono<String> fetchMoimRecommendation(Long userID) {
+       return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/embedding/search")
+                        .queryParam("userID", userID)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> fetchCategoryRecommendation(Long userID) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/embedding/search/category")
+                        .queryParam("userID", userID)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public void updateUserEmbedding(Long userID, String username, String prompt) {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("userId", userID);
+        jsonObject.addProperty("username", username);
+        jsonObject.addProperty("prompt", prompt);
+
+        webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/embedding")
+                        .build())
+                .bodyValue(jsonObject.toString())
+                .retrieve();
+    }
+
+    public void updateMoimEmbedding(Long moimID, String moimTitle, String prompt) {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("moimId", moimID);
+        jsonObject.addProperty("moimTitle", moimTitle);
+        jsonObject.addProperty("prompt", prompt);
+
+        webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/embedding/moim")
+                        .build())
+                .bodyValue(jsonObject.toString())
+                .retrieve();
+    }
+
+    public void updateCategoryEmbedding(Long categoryID, String categoryName, String prompt) {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("categoryId", categoryID);
+        jsonObject.addProperty("category", categoryName);
+        jsonObject.addProperty("prompt", prompt);
+
+        webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/embedding/category")
+                        .build())
+                .bodyValue(jsonObject.toString())
+                .retrieve();
+    }
+}
