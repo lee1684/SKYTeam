037999933ef313fc143a8bb538aaa33c69347134
SyingSHY <posti1x38@gmail.com>
2024-05-29 22:40:45 +0900

    fix: 후기 작성 여부 확인 로직 수정 (2)

--- back-end/src/main/java/kr/co/ssalon/domain/repository/DiaryRepository.java
+++ back-end/src/main/java/kr/co/ssalon/domain/repository/DiaryRepository.java
@@ -4,7 +4,10 @@ import kr.co.ssalon.domain.entity.Diary;
 import kr.co.ssalon.domain.entity.MemberMeeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
+    Optional<Diary> findByMemberMeetingId(Long memberMeetingId);
     Diary findByMemberMeeting(MemberMeeting memberMeeting);
 }
