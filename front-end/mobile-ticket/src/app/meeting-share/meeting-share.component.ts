589a5bbaab308c225e9b18e3b587c8ed09ffa7cc
sys7498 <sys7498@ajou.ac.kr>
2024-05-29 13:33:44 +0900

    fix: moved paraticipant position

---
+++ front-end/mobile-ticket/src/app/meeting-share/meeting-share.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-share',
+  standalone: true,
+  imports: [],
+  templateUrl: './meeting-share.component.html',
+  styleUrl: './meeting-share.component.scss'
+})
+export class MeetingShareComponent {
+
+}
