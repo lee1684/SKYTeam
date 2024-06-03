72e7fe83c0a1a26607e2a07d89c18e71655f245f
pileuszu <pileuszu@gmail.com>
2024-06-03 14:14:40 +0900

    feat: SearchController 파일 생성
    - SearchController.java: 컨트롤러 파일 생성

---
+++ back-end/src/main/java/kr/co/ssalon/web/controller/SearchController.java
@@ -0,0 +1,13 @@
+package kr.co.ssalon.web.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "검색")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+}
