be645901dec05b80cc58f4b8223181eb6d689982
pileuszu <pileuszu@gmail.com>
2024-06-03 16:02:34 +0900

    fix: 오류 수정을 위해 주석처리

--- back-end/src/main/java/kr/co/ssalon/domain/repository/MeetingRepository.java
+++ back-end/src/main/java/kr/co/ssalon/domain/repository/MeetingRepository.java
@@ -11,15 +11,13 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MeetingRepository extends JpaRepository<Meeting,Long>, MeetingRepositoryCustom, Pageable {
+public interface MeetingRepository extends JpaRepository<Meeting,Long>, MeetingRepositoryCustom {
 
     @Query("SELECT m FROM Meeting m WHERE m.isFinished = false")
     List<Meeting> findAllUnfinishedMeetings();
 
-    /*
+
     @Query("SELECT m FROM Meeting m WHERE m.title LIKE %:keyword% OR m.description LIKE %:keyword%")
     Page<Meeting> searchByTitleOrDescription(@Param("keyword") String keyword, Pageable pageable);
-
-     */
 
 }
