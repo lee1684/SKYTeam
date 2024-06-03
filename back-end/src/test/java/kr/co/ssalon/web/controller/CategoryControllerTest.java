9e4b7de62dec9d93f610b12276a9aacd8a638a56
pileuszu <pileuszu@gmail.com>
2024-06-03 19:07:50 +0900

    feat: categorycontoller 테스트 코드 추가

--- back-end/src/test/java/kr/co/ssalon/web/controller/CategoryControllerTest.java
+++ back-end/src/test/java/kr/co/ssalon/web/controller/CategoryControllerTest.java
@@ -1,4 +1,99 @@
 package kr.co.ssalon.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import kr.co.ssalon.domain.entity.Category;
+import kr.co.ssalon.domain.repository.CategoryRepository;
+import kr.co.ssalon.web.controller.annotation.WithCustomMockUser;
+import kr.co.ssalon.web.dto.CategoryHomeDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoryController.class)
 public class CategoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CategoryRepository categoryRepository;
+
+
+    @Test
+    @DisplayName("카테고리 전체 조회 API(GET /api/category/all) 테스트")
+    @WithCustomMockUser(username = "test")
+    public void 카테고리전체조회API() throws Exception {
+        // given
+
+        List<Category> categories = new ArrayList<>();
+
+        Category category1 = Category.builder()
+                .id(1L)
+                .name("categoryNameTest1")
+                .description("categoryDescriptionTest1")
+                .imageUrl("categoryImageUrlTest1")
+                .build();
+
+        Category category2 = Category.builder()
+                .id(2L)
+                .name("categoryNameTest2")
+                .description("categoryDescriptionTest2")
+                .imageUrl("categoryImageUrlTest2")
+                .build();
+
+        categories.add(category1);
+        categories.add(category2);
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/category/all").with(csrf()));
+
+        // then
+        resultActions.andExpect(status().isOk())
+        ;
+    }
+
+    @Test
+    @DisplayName("추천 카테고리 리스트 전체 조회 API(GET /api/category/recommend) 테스트")
+    @WithCustomMockUser(username = "test")
+    public void 추천카테고리전체조회API() throws Exception {
+
+    }
+
+
+    /*
+    @Operation(summary = "카테고리 전체 조회")
+    @ApiResponse(responseCode = "200", description = "카테고리 전체 조회 성공", content = {
+            @Content(schema = @Schema(implementation = Category.class))
+    })
+    @GetMapping("/api/category/all")
+
+    @Operation(summary = "추천 카테고리 리스트 전체 조회")
+    @ApiResponse(responseCode = "200", description = "카테고리 전체 조회 성공", content = {
+            @Content(schema = @Schema(implementation = CategoryHomeDTO.class))
+    })
+    @GetMapping("/api/category/recommend")
+
+     */
+
+
 }
---
+++ back-end/src/test/java/kr/co/ssalon/web/controller/QrControllerTest.java
@@ -0,0 +1,4 @@
+package kr.co.ssalon.web.controller;
+
+public class QrControllerTest {
+}
